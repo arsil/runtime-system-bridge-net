4593fa43366f2880455f2f4a8f1efc2a649d1a74
Arek.Waliszewski <arkadiusz.waliszewski@forcom.com.pl>
2013-09-24 13:33:11 +0200

    Added Linq.Expressions from Mono project.

---
+++ SystemBridge20To35/System.Linq.Expressions/MonoInternals.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace System.Linq.Expressions
+{
+	static class EmptyOf<T>
+	{
+		public static readonly T[] Instance = new T[0];
+	}
+
+	static class ReadOnlyCollectionOf<T>
+	{
+		public static readonly ReadOnlyCollection<T> Empty = new ReadOnlyCollection<T>(EmptyOf<T>.Instance);
+	}
+
+	internal static class Syfon
+	{
+		internal static ReadOnlyCollection<TSource> ToReadOnlyCollection<TSource>(this IEnumerable<TSource> source)
+		{
+			if (source == null)
+				return ReadOnlyCollectionOf<TSource>.Empty;
+
+			var ro = source as ReadOnlyCollection<TSource>;
+			if (ro != null)
+				return ro;
+
+			return new ReadOnlyCollection<TSource>(source.ToArray<TSource>());
+		}
+
+		static class EmptyOf<T>
+		{
+			public static readonly T[] Instance = new T[0];
+		}
+	}
+
+}
